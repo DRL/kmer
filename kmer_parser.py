a40dd722b6d0612e983dd4bae4a4825646a8da43
Dominik R Laetsch <dominik.laetsch@gmail.com>
2014-09-25 15:09:16 +0100

    ...

--- kmer_parser.py
+++ kmer_parser.py
@@ -4,15 +4,26 @@
 from __future__ import division
 import sys, argparse, os
 
+import numpy as np
+import matplotlib.pyplot as plt
+
 def parse_file(infile):
 	kmer_freq = {}
 	with open(infile) as fh:
 		for line in fh: 
 			count = int(line.rstrip("\n").split()[1])
 			kmer_freq[count] = kmer_freq.get(count, 0) + 1
+	#for key in sorted(kmer_freq):
+	#	print str(key) + "\t" + str(kmer_freq[key])
+	return kmer_freq
+
+def plot(kmer_freq):
 	for key in sorted(kmer_freq):
-		print str(key) + "\t" + str(kmer_freq[key])
+		plt.scatter(key, kmer_freq[key])
+	plt.show()
 
 if __name__ == "__main__":
 	kmer_file = sys.argv[1]
-	parse_file(kmer_file)
+	freq_dict = parse_file(kmer_file)
+
+	plot(freq_dict)
