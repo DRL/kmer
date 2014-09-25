7488f4b791b5bc60cf6018d10b0dfa223948e6d8
Dominik R Laetsch <dominik.laetsch@gmail.com>
2014-09-25 14:36:47 +0100

    ...

--- kmer_parser.py
+++ kmer_parser.py
@@ -1,14 +1,17 @@
 #!/usr/bin/env python
+# -*- coding: utf-8 -*-
 
 from __future__ import division
 import sys, argparse, os
 
 def parse_file(infile):
-	kmer_freq
+	kmer_freq = {}
 	with open(infile) as fh:
 		for line in fh: 
-			temp = line.rstrip("\n").split()
-			
+			count = int(line.rstrip("\n").split()[1])
+			kmer_freq[count] = kmer_freq.get(count, 0) + 1
+	for key in sorted(kmer_freq):
+		print str(key) + "\t" + str(kmer_freq[key])
 
 if __name__ == "__main__":
 	kmer_file = sys.argv[1]
