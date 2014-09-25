383ec6e400c90c5acf5f24f3529e5060836dd938
Dominik R. Laetsch <dominik.laetsch@gmail.com>
2014-09-25 13:58:17 +0100

    ...

---
+++ kmer_parser.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+
+from __future__ import division
+import sys, argparse, os
+
+def parse_file(infile):
+	kmer_freq
+	with open(infile) as fh:
+		for line in fh: 
+			temp = line.rstrip("\n").split()
+			
+
+if __name__ == "__main__":
+	kmer_file = sys.argv[1]
+	parse_file(kmer_file)
